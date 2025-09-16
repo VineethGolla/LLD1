61cbd3902b2c4128f675d68d557ffe2d0ab52d3a
vineeth <vineethvineeth614@gmail.com>
2025-09-16 11:29:54 -0500

    just checking observer design

--- designpatterns/ObserverDesign/Client.java
+++ designpatterns/ObserverDesign/Client.java
@@ -1,7 +1,10 @@
 package designpatterns.ObserverDesign;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
         WeatherStation station = new WeatherStation();
 
         Observer Phoneapp=new Phoneapp("iphone");
@@ -10,7 +13,13 @@ public class Client {
         station.registerObserver(Phoneapp);
         station.registerObserver(TV);
 
-        station.setweather(29.9F,5.0F);
+        System.out.println("update the temperature value :");
+        float temp=scn.nextFloat();
+
+        System.out.println("update the pressure value :");
+        float pressure=scn.nextFloat();
+
+        station.setweather(temp,pressure);
 
         station.removeObserver(Phoneapp);
 
